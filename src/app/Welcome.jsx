f1c219e993a62d800f042b5509671626da54e3b6
Muhammad Ayaad <muhammadayaad@pop-os.localdomain>
2023-12-20 17:15:55 +0500

    initial app setup

---
+++ src/app/Welcome.jsx
@@ -0,0 +1,23 @@
+
+import React from 'react'
+import './Welcome.css'
+
+function Welcome() {
+  return (
+    <>
+    <div className='welcome-popup'>
+     
+    <div className="main-prt">
+      <div className='connect'>
+        <h2>Connect Your Wallet</h2>
+        <p>By connecting your wallet, you agree to our Terms of Service and our Privacy Policy.</p>
+      </div>
+      
+    </div>
+        </div>
+    
+    </> 
+  )
+}
+
+export default Welcome
---
+++ src/app/page.js
@@ -0,0 +1,9 @@
+import Welcome from "./Welcome.jsx"
+
+export default function Home() {
+  return (
+   <>
+   <Welcome />
+   </>
+  )
+}
